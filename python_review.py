e0411365a3d091dab43e3f433a3bdbdf8a7cf1c9
leshemha24-meet <leshem.hadad@gmail.com>
2023-07-17 09:45:02 +0300

    Create python_review.py

---
+++ python_review.py
@@ -0,0 +1,42 @@
+def create_youtube_video(title, description):
+	yt_vid= {"title":title, "description":description, "likes":0, "dislikes":0, "comments":{}}
+	return yt_vid
+
+def like(yt_vid):
+	if "likes" in yt_vid:
+		yt_vid["likes"]= yt_vid["likes"+1]
+	return yt_vid
+
+def dislike(yt_vid):
+	if "dislikes" in yt_vid:
+		yt_vid["dislikes"]= yt_vid["dislikes"+1]
+	return yt_vid
+
+def add_comment(yt_vid):
+	username= input("what username would you want to have?")
+	comment_text= input("comment on the post!")
+	yt_vid["comments"][username]=comment_text
+	return yt_vid
+
+
+def main():
+
+	exit=False
+
+	print("Welcome to YouTube!")
+	tit = input("Enter a title of a video: ")
+	des = input("Enter the description of the video: ")
+	while exit==False:
+		print ("In order to like, click 1./nIn order to dislike, click 2./n In order to comment, click 3./n In order to exit, click 4.")
+		x= input("What would you want to do now?")
+		if x=='1':
+			like(yt_vid)
+		elif x=='2':
+			dislike(yt_vid)
+		elif x=='3':
+			add_comment(yt_vid)
+		elif x=='4':
+			exit=True
+		else:
+			print("Invalid choice. Try again.")
+		create_youtube_video(tit, des)
