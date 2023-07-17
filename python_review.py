3750c3a7c8123962e3a751953850d40dbfce10c0
leshemha24-meet <leshem.hadad@gmail.com>
2023-07-17 18:32:30 +0300

    Update python_review.py

--- python_review.py
+++ python_review.py
@@ -1,6 +1,18 @@
 def create_youtube_video(title, description):
-	yt_vid= {"title":title, "description":description, "likes":0, "dislikes":0, "comments":{}}
+	yt_vid= {"title":title, "description":description, "likes":0, "dislikes":0, "comments":{}, "hashtag":[]}
+	for i in range(5):
+		z= input("Add a word to describe the video: ")
+		yt_vid["hashtag"].append(z)
 	return yt_vid
+
+def similarity_to_video(dic1, dic2):
+	hash1= set(dic1["hashtag"])
+	hash2= set(dic2["hashtag"])
+	per= 0
+	for i in hash1:
+		if i in hash2:
+			per=per+100/len(hash1)
+	print("The videos are:", str(per)+"%")
 
 def like(yt_vid):
 	if "likes" in yt_vid:
@@ -22,7 +34,7 @@ def add_comment(yt_vid):
 exit=False
 
 print("Welcome to YouTube!")
-tit = input("Enter a title of a video: ")
+tit = input("Enter a title of the video: ")
 des = input("Enter the description of the video: ")
 y= create_youtube_video(tit, des)
 while exit==False:
@@ -41,4 +53,15 @@ while exit==False:
 		exit=True
 	else:
 		print("Invalid choice. Try again.")
+	ques = input("Would you want to check the similarities of two videos?")
+	if ques == "yes":
+		tit1 = input("Enter a title of the 1st video: ")
+		des1 = input("Enter the description of the 1st video: ")
+		vid1= create_youtube_video(tit1, des1)
+		tit2 = input("Enter a title of the 2nd video: ")
+		des2 = input("Enter the description of the 2nd video: ")
+		vid2= create_youtube_video(tit2, des2)
+		similarity_to_video(vid1, vid2)
+
+
 	
