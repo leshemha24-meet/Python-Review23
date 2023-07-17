2c33d6d56888f197131f0886ac17de94eef90826
leshemha24-meet <leshem.hadad@gmail.com>
2023-07-17 12:05:38 +0300

    Update python_review.py

--- python_review.py
+++ python_review.py
@@ -4,12 +4,12 @@ def create_youtube_video(title, description):
 
 def like(yt_vid):
 	if "likes" in yt_vid:
-		yt_vid["likes"]= yt_vid["likes"+1]
+		yt_vid["likes"]= yt_vid["likes"]+1
 	return yt_vid
 
 def dislike(yt_vid):
 	if "dislikes" in yt_vid:
-		yt_vid["dislikes"]= yt_vid["dislikes"+1]
+		yt_vid["dislikes"]= yt_vid["dislikes"]+1
 	return yt_vid
 
 def add_comment(yt_vid):
@@ -19,24 +19,26 @@ def add_comment(yt_vid):
 	return yt_vid
 
 
-def main():
+exit=False
 
-	exit=False
-
-	print("Welcome to YouTube!")
-	tit = input("Enter a title of a video: ")
-	des = input("Enter the description of the video: ")
-	while exit==False:
-		print ("In order to like, click 1./nIn order to dislike, click 2./n In order to comment, click 3./n In order to exit, click 4.")
-		x= input("What would you want to do now?")
-		if x=='1':
-			like(yt_vid)
-		elif x=='2':
-			dislike(yt_vid)
-		elif x=='3':
-			add_comment(yt_vid)
-		elif x=='4':
-			exit=True
-		else:
-			print("Invalid choice. Try again.")
-		create_youtube_video(tit, des)
+print("Welcome to YouTube!")
+tit = input("Enter a title of a video: ")
+des = input("Enter the description of the video: ")
+y= create_youtube_video(tit, des)
+while exit==False:
+	print ("In order to like, click 1./nIn order to dislike, click 2./n In order to comment, click 3./n In order to exit, click 4.")
+	x= input("What would you want to do now?")
+	if x=='1':
+		like(y)
+		print(y)
+	elif x=='2':
+		dislike(y)
+		print(y)
+	elif x=='3':
+		add_comment(y)
+		print(y)
+	elif x=='4':
+		exit=True
+	else:
+		print("Invalid choice. Try again.")
+	
